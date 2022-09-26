f51214b5557286742f39b1070b6c2df9ec0c2ee1
Clomclem <clemheiremans@gmail.com>
2022-09-26 20:01:57 -0300

    Added Radioactive javadoc

--- src/main/java/me/clomclem/radiochaos/radioactive/Radioactive.java
+++ src/main/java/me/clomclem/radiochaos/radioactive/Radioactive.java
@@ -2,6 +2,10 @@ package me.clomclem.radiochaos.radioactive;
 
 import net.minecraft.world.damagesource.DamageSource;
 
+/**
+ * An interface for anything that has or can have radioactive
+ * properties.
+ */
 public interface Radioactive {
     // Once every 100 ticks, the player gets damaged by radiation
     int radiationTicks = 100;
